7e8bd186a25f748711791cc96ec67eed2fab05f8
Sigrid <sigridxyx@gmail.com>
2021-03-09 15:44:28 +0800

    Update __init__.py

---
+++ src/onqg/models/encoders/__init__.py
@@ -0,0 +1,5 @@
+from onqg.models.encoders.RNNEncoder import RNNEncoder
+from onqg.models.encoders.TransfEncoder import TransfEncoder
+from onqg.models.encoders.GATEncoder import GATEncoder
+from onqg.models.encoders.GGNNEncoder import GGNNEncoder
+# from onqg.models.encoders.GCNEncoder import GCNEncoder
